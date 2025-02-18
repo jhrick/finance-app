d2561d16c259971ddc40ad779edf8b0dcdcfb715
jhrick <hj.rickcp@gmail.com>
2025-02-18 09:45:48 -0300

    🗃️ chore: add MySQL connection

--- app/protected/config/database.php
+++ app/protected/config/database.php
@@ -1,14 +1,12 @@
 <?php
 
-// This is the database connection configuration.
-return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__."/../../../../"));
+$dotenv->load();
+
+return [
+	'connectionString' => $_ENV['DATABASE_URL'],
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => $_ENV['DATABASE_USER'],
+	'password' => $_ENV['DATABASE_PASSWORD'],
 	'charset' => 'utf8',
-	*/
-);
+];
